b7664f8630f718d1b2b37d620c8d6924c8a9baac
w0daz <tamexplora@gmail.com>
2025-02-17 22:51:41 +0200

    final touches

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
+import { Routes, Route } from 'react-router-dom';
 import Root from './routes/Root';
 import Dashboard from './routes/Dashboard';
 import Tasks from './routes/Tasks';
@@ -8,15 +6,13 @@ import ErrorPage from './routes/ErrorPage';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Root />}>
-          <Route index element={<Dashboard />} />
-          <Route path="tasks" element={<Tasks />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Root />}>
+        <Route index element={<Dashboard />} />
+        <Route path="tasks" element={<Tasks />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    </Routes>
   );
 };
 
--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,11 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
-    <nav className="navigation">
-      <ul>
-        <li><Link to="/">Dashboard</Link></li>
-        <li><Link to="/tasks">Tasks</Link></li>
+    <nav className="sidebar">
+      <ul className="nav-list">
+        <li>
+          <Link to="/" className="nav-link">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/tasks" className="nav-link">Tasks</Link>
+        </li>
       </ul>
     </nav>
   );
